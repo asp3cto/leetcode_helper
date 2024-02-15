b021853e9be906c19427b64ee7a9abf1cf0e692d
Artem Kudrin <akudrin2002@gmail.com>
2024-02-15 03:34:33 +0300

    added register test

--- auth_server/tests/conftest.py
+++ auth_server/tests/conftest.py
@@ -8,35 +8,41 @@ from httpx import AsyncClient
 import pytest
 
 from core.models.helper import pg_db_helper
-from core.models import Base
+from core.models import Base, User
+from core.config import settings
 from app import app
 
-from sqlalchemy import String, LargeBinary, sql
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import NullPool, String, LargeBinary, sql
+from sqlalchemy.orm import Mapped, mapped_column, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-class TestUser(Base):
-    __tablename__ = "test_user"
-    username: Mapped[str] = mapped_column(String(30), unique=True)
-    email: Mapped[str | None]
-    is_active: Mapped[bool] = mapped_column(server_default=sql.true())
-    hashed_password: Mapped[bytes] = mapped_column(LargeBinary())
+TEST_DB_URL = f"postgresql+asyncpg://auth:{settings.postgres_password}@postgres_test/auth"
 
+engine_test = create_async_engine(TEST_DB_URL, poolclass=NullPool)
+async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+
+async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+app.dependency_overrides[pg_db_helper.scoped_session_dependency] = override_get_async_session
+User.metadata.bind = engine_test
 
 @pytest.fixture(autouse=True, scope="session")
 async def prepare_database():
-    async with pg_db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    async with engine_test.begin() as conn:
+        await conn.run_sync(User.metadata.create_all)
     yield
-    async with pg_db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all, tables=[TestUser.__table__])
+    async with engine_test.begin() as conn:
+        await conn.run_sync(User.metadata.drop_all)
 
 # SETUP
-@pytest.fixture(scope="session")
-def event_loop(request):
-    """Create an instance of the default event loop for each test case."""
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
+# @pytest.fixture(scope="session")
+# def event_loop(request):
+#     """Create an instance of the default event loop for each test case."""
+#     loop = asyncio.get_event_loop_policy().new_event_loop()
+#     yield loop
+#     loop.close()
 
 client = TestClient(app)
 
---
+++ auth_server/tests/test_auth.py
@@ -0,0 +1,29 @@
+"""
+Module with tests for auth server
+"""
+from fastapi import status
+from httpx import AsyncClient
+from app import app
+
+from conftest import client
+
+
+async def test_root(ac: AsyncClient):
+    expected_response = {"detail": "home"}
+    
+    response = await ac.get("/auth/")
+    
+    assert expected_response == response.json()
+
+
+async def test_register_unique(ac: AsyncClient):
+    expected_response_status_code = status.HTTP_201_CREATED
+
+    response = await ac.post("/auth/register/", json={
+        "username": "unique_sloi",
+        "email": "test@example.com",
+        "password": "unique_user_passwd"
+    })
+
+    assert response.status_code == expected_response_status_code
+    

e2c285f38862b78c3b41fb643e00f1c25971b242
Artem Kudrin <akudrin2002@gmail.com>
2024-02-19 01:25:03 +0300

    added hints in crud

--- info_server/api_v1/problems/crud.py
+++ info_server/api_v1/problems/crud.py
@@ -5,9 +5,9 @@ from fastapi_pagination.ext.beanie import paginate
 from core.models import Problem
 
 
-async def find_problems_by_filter_with_pagination(filters: dict):
+async def find_problems_by_filter_with_pagination(filters: dict) -> list:
     return await paginate(Problem.find_many(filters))
 
 
-async def find_top_problems_with_pagination():
+async def find_top_problems_with_pagination() -> list:
     return await paginate(Problem.find_many(Problem.id <= 100))
